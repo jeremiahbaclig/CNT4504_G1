dbdf34785c2b94b9251763157e88c26b26664f5b
Jeremiah Baclig <JeremiahBaclig@gmail.com>
2021-02-03 18:01:06 -0500

    added projects

---
+++ Project1/client/cnt4504/MultiClient.java
@@ -0,0 +1,8 @@
+package cnt4504;
+
+public class MultiClient {
+	public static void main(String args[]) {
+       System.out.println("Test client.\n");
+    }
+}
+
---
+++ Project1/src/cnt4504/SingleServer.java
@@ -0,0 +1,7 @@
+package cnt4504;
+
+public class SingleServer {
+	public static void main(String args[]) {
+       System.out.println("Test server.\n");
+    }
+}
