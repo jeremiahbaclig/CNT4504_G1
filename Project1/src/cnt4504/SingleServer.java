1e0a6d44ba94bb4c8b5de615ed25a377ec0b8411
Jeremiah Baclig <JeremiahBaclig@gmail.com>
2021-02-20 20:08:13 -0500

    prototype for client (needs more cases and multithreading/exception handling)

--- Project1/src/cnt4504/SingleServer.java
+++ Project1/src/cnt4504/SingleServer.java
@@ -3,13 +3,24 @@ package cnt4504;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 public class SingleServer {
 	public static void main(String args[]) {
-		if (args.length < 1) 
+		if (args.length < 1) {
 			System.out.println("USAGE: java SingleServer.java <port number>");
+			return;
+		}
 		
 		int port = Integer.parseInt(args[0]);
+		
+//		try {
+//	        InetAddress myHost = InetAddress.getLocalHost();
+//	        System.out.println(myHost.getHostName());
+//        } catch(UnknownHostException ex) {
+//        	return;
+//        }
 		
 	    try (ServerSocket serverSocket = new ServerSocket(port)) {
 		 
@@ -19,20 +30,48 @@ public class SingleServer {
 		    while (true) {
 		    	Socket socket = serverSocket.accept();
 		    	
+		    	// read data from client
 		    	InputStream input = socket.getInputStream();
 		    	BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 		    	
+		    	// send data to client
 		        OutputStream output = socket.getOutputStream();
 		        PrintWriter writer = new PrintWriter(output, true);
-		 
-		        String text;
+
+		        
+		        int choice = 0;
 		        do {
-                    text = reader.readLine();
-                    // do things with text based on client input
-                    // String reverseText = new StringBuilder(text).reverse().toString();
-                    // writer.println("Server: " + reverseText);
-                    
-                } while (!text.equals("7"));
+			        choice = reader.read();
+		        	
+		        	switch(choice) {
+		    		case 49: // print server date and time (1)
+		    			writer.println(new Date().toString());
+		    			break;
+		    		case 50: // print server up time (2)
+		    			RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+		    			writer.println("Uptime in ms: " + rb.getUptime());
+		    			break;
+		    		case 51:
+		    			// Do something for memory use (3)
+		    			break;
+		    		case 52:
+		    			// Do something for netstat (4)
+		    			break;
+		    		case 53:
+		    			// Do something for current users (5)
+		    			break;
+		    		case 54:
+		    			// Do something for running processes (6)
+		    			break;
+		    		case 55:
+		    			// Quit client thread (7)
+		    			socket.close();
+		    			break;
+		    		default:
+		    			// Do something for input error
+		    			break;	
+		        	}
+                } while (choice != 7);
 	            
 	            socket.close();
 	        }
